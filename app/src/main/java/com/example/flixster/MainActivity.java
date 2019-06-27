e40cfffbfbead91e4d5cf4902e43050e95e32f37
Chiara Darnton <cmdarnton@fb.com>
2019-06-27 15:03:52 -0700

    Initial commit

---
+++ app/src/main/java/com/example/flixster/MainActivity.java
@@ -0,0 +1,142 @@
+package com.example.flixster;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.flixster.models.Config;
+import com.example.flixster.models.Movie;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MainActivity extends AppCompatActivity {
+
+    //CONSTANTS
+    public final static String API_BASE_URL = "https://api.themoviedb.org/3";
+    public final static String API_KEY_PARAM = "api_key";
+    //tag for logging from this activity
+    public final static String TAG = "MainActivity";
+
+
+    //INSTANCE FIELDS
+    AsyncHttpClient client;
+
+    ArrayList<Movie> movies;
+    RecyclerView rvMovies;
+    MovieAdapter adapter;
+    Config config;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //init the client
+        client = new AsyncHttpClient();
+
+        movies = new ArrayList<>();
+        //init the adapter -- movies array cannot be reinitialized after this point
+        adapter = new MovieAdapter(movies);
+        //resolve the recycler view and connect a layout manager and the adapter
+        rvMovies = (RecyclerView) findViewById(R.id.rvMovies);
+        rvMovies.setLayoutManager(new LinearLayoutManager(this));
+        rvMovies.setAdapter(adapter);
+
+        //get the configuration on app creation
+        getConfiguration();
+
+    }
+
+    //get the list of current movies from API
+    private void getNowPlaying() {
+        String url = API_BASE_URL + "/movie/now_playing";
+        RequestParams params = new RequestParams();
+        params.put(API_KEY_PARAM, getString(R.string.api_key));
+        client.get(url, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                //load results into movies list
+                try {
+                    JSONArray results = response.getJSONArray("results");
+                    //iterate through result set and create Movie objects
+                    for (int i = 0; i < results.length(); i++) {
+                        Movie movie = new Movie(results.getJSONObject(i));
+                        movies.add(movie);
+                        //notify adapter that a row was added
+                        adapter.notifyItemInserted(movies.size() - 1);
+                    }
+                    Log.i(TAG, String.format("Loaded %s movies", results.length()));
+                } catch (JSONException e) {
+                    logError("Failed to parse now playing movies", e, true);
+
+                }
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                logError("Failed to get data from now playing endpoint", throwable, true);
+            }
+        });
+    }
+
+    //get configuration from the API
+    private void getConfiguration() {
+        String url = API_BASE_URL + "/configuration";
+        RequestParams params = new RequestParams();
+        params.put(API_KEY_PARAM, getString(R.string.api_key));
+        //exectute a GET request expecting a JSON object response
+        client.get(url, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+
+                    config = new Config(response);
+                    Log.i(TAG, String.format("Loaded configuration with imageBaseUrl %s and posterSize %s", config.getImageBaseUrl(), config.getPosterSize()));
+                    adapter.setConfig(config);
+                    //get the now playing list
+                    getNowPlaying();
+
+                } catch (JSONException e) {
+                    logError("Failed parsing configuration", e, true);
+                }
+
+
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                    logError("Failed getting configuration", throwable, true);
+            }
+        });
+    }
+
+    //handle errors, log and alert user
+    private void logError(String message, Throwable error, boolean alertUser) {
+        //always log the error
+        Log.e(TAG, message, error);
+        //alert the user to avoid silent errors
+        if (alertUser) {
+            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+        }
+    }
+
+}
+
+
---
+++ app/src/main/java/com/example/flixster/MovieAdapter.java
@@ -0,0 +1,108 @@
+package com.example.flixster;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.flixster.models.Config;
+import com.example.flixster.models.Movie;
+
+import java.util.ArrayList;
+
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+
+    ArrayList<Movie> movies;
+    Config config;
+    Context context;
+
+    public MovieAdapter(ArrayList<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public void setConfig(Config config) {
+        this.config = config;
+    }
+
+    //creates and inflates a new view
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //get context and create the inflater
+        context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        //create view using the item_movie layout
+        View movieView = inflater.inflate(R.layout.item_movie, parent, false);
+        //return a new ViewHolder
+        return new ViewHolder(movieView);
+    }
+
+    //binds an inflated view to a new item
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        //get movie data at the specified position
+        Movie movie = movies.get(position);
+        //populate the view with movie data
+        holder.tvTitle.setText(movie.getTitle());
+        holder.tvOverview.setText(movie.getOverview());
+
+        //determine current orientation
+        boolean isPortrait = context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
+
+        String imageUrl = null;
+
+        if (isPortrait) {
+            imageUrl = config.getImageUrl(config.getPosterSize(), movie.getPosterPath());
+        } else {
+            imageUrl = config.getImageUrl(config.getBackdropSize(), movie.getBackdropPath());
+        }
+
+        //get correct placeholder and imageview w/ ternary
+        int placeholderId = isPortrait ? R.drawable.flicks_movie_placeholder : R.drawable.flicks_backdrop_placeholder;
+        ImageView imageView = isPortrait ? holder.ivPosterImage : holder.ivBackdropImage;
+
+        Glide.with(context)
+                .load(imageUrl)
+                .bitmapTransform(new RoundedCornersTransformation(context, 25, 0))
+                .placeholder(placeholderId)
+                .error(placeholderId)
+                .into(imageView);
+
+
+    }
+
+    //returns the total number of items in the list - MUST remember it's not zero
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    //create viewholder as a static inner class
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        //track view objects
+        ImageView ivPosterImage;
+        ImageView ivBackdropImage;
+        TextView tvTitle;
+        TextView tvOverview;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            //lookup view objects by id
+            tvOverview = (TextView) itemView.findViewById(R.id.tvOverview);
+            tvTitle = (TextView) itemView.findViewById(R.id.tvTitle);
+            ivPosterImage = (ImageView) itemView.findViewById(R.id.ivPosterImage);
+            ivBackdropImage = (ImageView) itemView.findViewById(R.id.ivBackdropImage);
+
+        }
+    }
+}
